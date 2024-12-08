ae5b40cf3f9a4c321e8b39967c8f80e7b7acccbc
lianet <jack.tolmak@yandex.ru>
2024-12-08 13:02:18 +0300

    commit

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -2,75 +2,108 @@ import { bodyLocking } from "./locking.js";
 
 export const popup = () => {
 
-
 	document.addEventListener('click', (e) => {
-
 		const target = e.target;
 
 		if (target.closest('[data-modal]')) {
+			e.preventDefault();
+
+
+
+
 			const targetBtn = target.closest('[data-modal]');
-			const popupName = targetBtn.getAttribute('href').replace("#", '');
+			const popupName = targetBtn.tagName === 'A' && targetBtn.hasAttribute('href')
+				? targetBtn.getAttribute('href').replace("#", '')
+				: targetBtn.getAttribute('data-href');
+
+			console.log('click', popupName);
+
 			const modal = document.getElementById(popupName);
+
 			if (modal) {
 				popupOpen(modal, targetBtn);
 			}
-			e.preventDefault();
-
 		}
 
 		if (target.closest('[data-close-modal]')) {
-			popupClose(target.closest('.popup'));
 			e.preventDefault();
+			const activePopup = target.closest('.popup');
+			if (activePopup) popupClose(activePopup);
 		}
 
 		if (target.classList.contains('popup__content')) {
-			popupClose(target.closest('.popup'));
+			const activePopup = target.closest('.popup');
+			if (activePopup) popupClose(activePopup);
+		}
+	});
+
+	document.addEventListener('keydown', (e) => {
+		const activePopup = document.querySelector('.popup.open');
+
+		if (e.key === "Escape" && activePopup) {
+			popupClose(activePopup);
 		}
 
+		if (e.key === "Enter" && activePopup) {
+			e.preventDefault();
+		}
 	});
 
 	function popupOpen(modal, modalBtn) {
 		const popupActive = document.querySelector('.popup.open');
+
 		if (popupActive) {
-			popupClose(popupActive);
+			popupClose(popupActive, false);
 		} else {
 			bodyLocking();
 		}
+
 		modal.classList.add('open');
-		modal.setAttribute('aria-hidden', false);
-		modal.setAttribute('tabindex', '-1');
-		modalBtn.setAttribute('aria-expanded', false);
-		modalBtn.setAttribute('data-modal', 'open');
+		setAttributes(modal, {
+			'aria-hidden': 'false',
+			'tabindex': '-1'
+		});
+
+		setAttributes(modalBtn, {
+			'aria-expanded': 'true',
+			'data-modal': 'open'
+		});
+
+		modal.dispatchEvent(new CustomEvent('popupOpen', { detail: { modal, modalBtn } }));
 	}
 
-	function popupClose(popupActive) {
-
+	function popupClose(modal, unlockBody = true) {
 		const modalBtn = document.querySelector('[data-modal="open"]');
 
-		popupActive.classList.remove('open');
-		popupActive.removeAttribute('tabindex');
-		popupActive.setAttribute('aria-hidden', true);
+		modal.classList.remove('open');
+		setAttributes(modal, {
+			'aria-hidden': 'true',
+			'tabindex': null
+		});
 
-		modalBtn.setAttribute('aria-expanded', true);
-		modalBtn.setAttribute('data-modal', '');
+		if (modalBtn) {
+			setAttributes(modalBtn, {
+				'aria-expanded': 'false',
+				'data-modal': ''
+			});
+		}
 
-		bodyLocking();
+		if (unlockBody) {
+			bodyLocking();
+		}
+
+
+		modal.dispatchEvent(new CustomEvent('popupClose', { detail: { modal } }));
 	}
 
-
-	document.addEventListener('keydown', function (e) {
-		if (e.key === "Escape") {
-			const popupActive = document.querySelector('.popup.open');
-			if (popupActive) {
-				popupClose(popupActive);
+	function setAttributes(element, attributes) {
+		Object.keys(attributes).forEach(key => {
+			const value = attributes[key];
+			if (value === null) {
+				element.removeAttribute(key);
+			} else {
+				element.setAttribute(key, value);
 			}
-		}
-		if (e.key === "Enter") {
-			const popupActive = document.querySelector('.popup.open');
-			if (popupActive) {
-				e.preventDefault()
-			}
-		}
-	});
-}
-
+		});
+	}
+};
